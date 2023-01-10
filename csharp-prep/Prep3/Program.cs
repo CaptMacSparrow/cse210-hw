31b5abfa9d8b6d05009cb03c78496ddf2eb6a1c7
Your Name <you@example.com>
2023-01-10 13:37:06 -0700

    magic number generator and guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       // Console.Write("What is the magic number? ");
+       // int MagicNumber = int.Parse(Console.ReadLine());
+        
+        Random randomGenerator = new Random();
+        int MagicNumber = randomGenerator.Next(1, 101);
+        
+        int guess = -1;
+
+        while (guess != MagicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > MagicNumber)
+            {
+                Console.Write("Lower. ");
+            }
+            else if (guess < MagicNumber)
+            {
+                Console.Write("Higher. ");
+            }
+            else
+            {
+                Console.Write("You guessed it!");
+            }
+        }
     }
 }
