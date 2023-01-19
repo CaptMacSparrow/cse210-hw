1b2263522eafcfbdc2df8a03e5caa71549ba6a18
Your Name <you@example.com>
2023-01-18 19:23:05 -0700

    abstraction with classes and methods

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2018;
+        job1._endYear = 2021; 
+        //{
+        //    Console.WriteLine($"{job1._jobTitle} {job1._company}");
+        //}
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+        //{
+        //  Console.WriteLine($"{job2._jobTitle} {job2._company}");
+        //}
+        //job1.DisplayJobDetails();
+        //job2.DisplayJobDetails();
+        Resume myResume = new Resume();
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume._name = "Jake Kiehl";
+
+        //Console.WriteLine($"{myResume._jobs[0]._jobTitle}");
+        myResume.Display();
     }
 }
