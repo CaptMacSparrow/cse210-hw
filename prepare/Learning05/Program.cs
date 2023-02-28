939ec33164b22bb324f219344d8f162dbdf691d5
Your Name <you@example.com>
2023-02-28 14:34:43 -0700

    Polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>(); 
+
+        Square s1 = new Square("Blue", 2);
+        //square1.GetColor();
+        //square1.GetArea();
+
+        //Console.WriteLine($"Color: {square1.GetColor()}. Area: {square1.GetArea()}");
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Purple", 2, 3);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Brown", 5);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"Color: {color}, Area: {area}. ");
+        } 
     }
 }
